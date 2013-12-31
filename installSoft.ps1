45a6a4c2d2334b3c07e5bc0659862a4e0feef861
Ma3yTa <maksimetsa@gmail.com>
2013-12-31 14:33:45 +0200

    Update installSoft.ps1

--- installSoft.ps1
+++ installSoft.ps1
@@ -1,7 +1,9 @@
 # http://boxstarter.org/package/url?
+
 #####################
 # BEGIN CONFIGURATION
 #####################
+
 #region WindowsConfig
   Disable-UAC
   Import-Module $env:systemdrive\chocolatey\chocolateyinstall\helpers\chocolateyInstaller.psm1
@@ -15,6 +17,7 @@
   Move-LibraryDirectory "My Pictures" "D:\Media\Фото" -DoNotMoveOldContent
   Move-LibraryDirectory "My Video" "D:\Media" -DoNotMoveOldContent
 #endregion
+
 #region VS2013
   cinst VisualStudio2013Professional -InstallArguments "WebTools SQL"
   Install-ChocolateyPinnedTaskBarItem "$env:windir\system32\mstsc.exe"
@@ -22,16 +25,19 @@
   Install-ChocolateyVsixPackage WebEssentials2013 http://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/23/WebEssentials2013.vsix
   Install-ChocolateyVsixPackage Mindscape.WebWorkbench.Integration.10 http://visualstudiogallery.msdn.microsoft.com/2b96d16a-c986-4501-8f97-8008f9db141a/file/53962/58/Mindscape.WebWorkbench.Integration.10.vsix
 #endregion
+
 #region IISConfig
   cinst IIS-WebServerRole -source windowsfeatures
   cinst IIS-HttpCompressionDynamic -source windowsfeatures
   cinst IIS-ManagementConsole -source windowsfeatures
   cinst Microsoft-Hyper-V-All -source windowsFeatures
 #endregion
+
 #region AnotherSoft
   $client = New-Object system.net.WebClient;
   $client.DownloadFile("https://raw.github.com/Ma3yTa/osprepare/master/packages.config","packages.config");
   cinst packages.config
 #endregion
+
 #Set-ExecutionPolicy AllSigned
 #Enable-UAC
