bd3c4cc0b9033e6dfde57a8902a0a37bff997842
Ma3yTa <maksimetsa@gmail.com>
2013-12-31 14:34:27 +0200

    Update installSoft.ps1

--- installSoft.ps1
+++ installSoft.ps1
@@ -16,12 +16,12 @@
   Move-LibraryDirectory "My Music" "D:\Media\Music" -DoNotMoveOldContent
   Move-LibraryDirectory "My Pictures" "D:\Media\Фото" -DoNotMoveOldContent
   Move-LibraryDirectory "My Video" "D:\Media" -DoNotMoveOldContent
+  Install-ChocolateyPinnedTaskBarItem "$env:windir\system32\mstsc.exe"
+  Install-ChocolateyPinnedTaskBarItem "$env:windir\system32\WindowsPowerShell\v1.0\powershell.exe"
 #endregion
 
 #region VS2013
   cinst VisualStudio2013Professional -InstallArguments "WebTools SQL"
-  Install-ChocolateyPinnedTaskBarItem "$env:windir\system32\mstsc.exe"
-  Install-ChocolateyPinnedTaskBarItem "$env:windir\system32\WindowsPowerShell\v1.0\powershell.exe"
   Install-ChocolateyVsixPackage WebEssentials2013 http://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/23/WebEssentials2013.vsix
   Install-ChocolateyVsixPackage Mindscape.WebWorkbench.Integration.10 http://visualstudiogallery.msdn.microsoft.com/2b96d16a-c986-4501-8f97-8008f9db141a/file/53962/58/Mindscape.WebWorkbench.Integration.10.vsix
 #endregion
